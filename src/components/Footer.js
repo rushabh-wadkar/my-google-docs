0c5af151f2a4a45748e421a377b7034689bd352b
rushabh <rushabh6792@gmail.com>
2021-04-24 17:02:36 +0530

    Client code added

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer>
+      Made with ❤️ by{"  "}
+      <a
+        href="https://www.linkedin.com/in/rushabh-wadkar"
+        target="_blank"
+        referrerPolicy="no-referrer"
+        rel="noreferrer"
+      >
+        Rushabh Wadkar
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
